fdf22166a91f072034f38b12709b21dca9293267
NareshKeerapatla <39722428+NareshKeerapatla@users.noreply.github.com>
2025-11-18 17:57:01 +0530

    Update provider.tf

--- project-vpc-ec2/provider.tf
+++ project-vpc-ec2/provider.tf
@@ -1,13 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "6.13.0"
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  # Configuration options
   region = "ap-south-1"
 }
+
